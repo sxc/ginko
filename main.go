a49f6904eaa1a6b12447f27c544fe24aa2e54a35
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-08-11 22:02:43 +0800

    post to accounts and show accounts id

--- main.go
+++ main.go
@@ -7,16 +7,23 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/sxc/ginko/api"
 	db "github.com/sxc/ginko/db/sqlc"
-	"github.com/sxc/ginko/util"
+	// "github.com/sxc/ginko/util"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:password@localhost:5432/ginko?sslmode=disable"
+	ServerAddress = "0.0.0.0:3000"
 )
 
 func main() {
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
+	// config, err := util.LoadConfig(".")
+	// if err != nil {
+	// 	log.Fatal("cannot load config:", err)
+	// }
 
-	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	// conn, err := sql.Open(config.DBDriver, config.DBSource)
+	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +31,8 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(config.ServerAddress)
+	// err = server.Start(config.ServerAddress)
+	err = server.Start(ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
